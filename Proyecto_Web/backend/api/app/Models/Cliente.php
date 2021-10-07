e94e3e7f599541176d5f304688fd49cde773c1a1
NidiaRosa <rosabittar0118@hotmail.com>
2021-10-07 01:02:06 -0500

    Conexion bases de datos
    Conexion bases de datos

--- Proyecto_Web/backend/api/app/Models/Cliente.php
+++ Proyecto_Web/backend/api/app/Models/Cliente.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    use HasFactory;
+    protected $table = 'clientes';
+    protected $primaryKey ='doc';
+
+    public function reservas(){
+        return $this->hasMany(reserva::class);
+    }
 }
--- Proyecto_Web/backend/api/app/Models/Reserva.php
+++ Proyecto_Web/backend/api/app/Models/Reserva.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
-    use HasFactory;
+    protected $table = 'reservas';
+
+    public function cliente(){
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function habitacion(){
+        return $this->belongsTo(Habiacion::class);
+    }
 }
