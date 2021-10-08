fd605673d1274332681363287ea51e0bc120447d
NidiaRosa <rosabittar0118@hotmail.com>
2021-10-07 19:43:15 -0500

    actualizacion api
    actualizacion api

--- Proyecto_Web/backend/api/app/Models/Cliente.php
+++ Proyecto_Web/backend/api/app/Models/Cliente.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Cliente extends Model
 {
+    use Notifiable;
+    public $timestamps = false;
+
     protected $table = 'clientes';
     protected $primaryKey ='doc';
+
+    protected $fillable = ['doc', 'nombre', 'apellidos', 'correo', 'clave', 'registro'];
 
     public function reservas(){
         return $this->hasMany(reserva::class);
