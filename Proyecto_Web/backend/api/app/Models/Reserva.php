0db854e17a75ae2fcbd6c79fd680f8f5b710d066
NidiaRosa <rosabittar0118@hotmail.com>
2021-10-11 23:44:01 -0500

    acts

--- Proyecto_Web/backend/api/app/Models/Reserva.php
+++ Proyecto_Web/backend/api/app/Models/Reserva.php
@@ -7,13 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reserva extends Model
 {
-    protected $table = 'reservas';
-
-    public function cliente(){
-        return $this->belongsTo(Cliente::class);
-    }
-
-    public function habitacion(){
-        return $this->belongsTo(Habiacion::class);
-    }
+    
 }
