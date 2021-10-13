cb1aa24e40f1de709a8a039b21470b679bab9222
NidiaRosa <rosabittar0118@hotmail.com>
2021-10-12 20:17:05 -0500

    update api

--- Proyecto_Web/backend/api/routes/api.php
+++ Proyecto_Web/backend/api/routes/api.php
@@ -13,10 +13,10 @@ Route::get('/Habitacion/search',[HabitacionController::class, 'search']);
 /* Route::post('/habitacion/filter',[HabitacionController::class, 'filter']); */
 
 Route::group(['middleware' => ['jwt.verify']], function() {
-    Router::get('/Cliente',[ClienteController::class, 'showAll']);
-    Router::post('/Cliente/update',[ClienteController::class, 'update']);
-    Router::post('/Reservacion/create',[ReservaController::class, 'create']);
-    Router::post('/Reservacion/history',[ReservaController::class, 'showByCliente']);
+    Route::get('/Cliente', [ClienteController::class, 'showAll']);
+    Route::post('/Cliente/update', [ClienteController::class, 'update']);
+    Route::post('/Reserva/create', [ReservaController::class, 'create']);
+    Route::post('/Reserva/history', [ReservaController::class, 'showByCliente']);
 });
 
 /* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
